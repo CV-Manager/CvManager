ff787a9d1c6a3f74a4334a94cf5bc151825145b7
KennyVell <kenneth.geme1@gmail.com>
2024-07-16 18:43:35 -0500

    front login changes

--- ViewModels/RegisterVM.cs
+++ ViewModels/RegisterVM.cs
@@ -2,6 +2,7 @@ namespace CvManager.ViewModels
 {
     public class RegisterVM
     {
+        public string? Name { get; set; }
         public string? Country { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
